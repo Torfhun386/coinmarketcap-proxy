97fdeae1e5d3f3ff23fdbb64eb0efdee70eca406
Torfhun386 <torfhun.maharaja@gmail.com>
2025-07-11 19:33:12 +0700

    Update index.js

--- index.js
+++ index.js
@@ -28,8 +28,8 @@ app.get("/price/:chain/:token", async (req, res) => {
       headless: true,
       args: [
         '--no-sandbox',
-        '--disable-setuid-sandbox',
-      ],
+        '--disable-setuid-sandbox'
+      ]
     });
     const page = await browser.newPage();
     await page.goto(url, { waitUntil: "networkidle2", timeout: 20000 });
